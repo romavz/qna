de3018ca8ee0273a2a095deeed9c3d6200766e5a
Roman <zworkbox@yandex.ru>
2020-01-24 11:46:01 +0300

    p4 add Answer model with specs

---
+++ db/migrate/20200123215256_create_answers.rb
@@ -0,0 +1,11 @@
+class CreateAnswers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :answers do |t|
+      t.text :body
+
+      t.timestamps
+    end
+
+    add_reference :answers, :question, foreign_key: true, null: false, on_delete: :cascade
+  end
+end
